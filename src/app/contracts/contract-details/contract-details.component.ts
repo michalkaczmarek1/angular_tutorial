55065998e5ee8d4d588e8fe9d491fc7e431c4737
Michal Kaczmarek <mkaczmarek@nicmangroup.com>
2020-02-21 16:09:14 -0700

    Router - parametry i straznicy

---
+++ src/app/contracts/contract-details/contract-details.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ContractService } from '../contract.service';
+import { Contract } from '../model';
+
+@Component({
+  selector: 'app-contract-details',
+  templateUrl: './contract-details.component.html',
+  styleUrls: ['./contract-details.component.css']
+})
+export class ContractDetailsComponent implements OnInit {
+
+  contract: Contract;
+
+  constructor(
+    private contractService: ContractService,
+    private activatedRoute: ActivatedRoute
+  ) { }
+
+  ngOnInit() {
+    this.activatedRoute.params.subscribe(params => {
+      const id = parseInt(params['id']);
+      this.contractService.getContract(id).subscribe(contract => {
+        this.contract = contract;      
+      })
+    })
+  }
+
+}
---
+++ src/app/core/auth.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  isLogged: boolean;
+
+  constructor() { }
+
+  login(){
+    this.isLogged = true;
+  }
+
+  logout(){
+    this.isLogged = false;
+  }
+}
